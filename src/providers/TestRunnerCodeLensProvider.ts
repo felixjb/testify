98b89bad031cf1ff47fc17d43619158357737c4d
Sascha Dais <41467575+roggenbrot@users.noreply.github.com>
2018-07-22 21:27:26 +0200

    Check package.json for test framework dependencies

--- src/providers/TestRunnerCodeLensProvider.ts
+++ src/providers/TestRunnerCodeLensProvider.ts
@@ -1,8 +1,16 @@
-import { CodeLens, CodeLensProvider, TextDocument, workspace } from "vscode";
+import {
+  CodeLens,
+  CodeLensProvider,
+  TextDocument,
+  window,
+  workspace,
+  WorkspaceFoldersChangeEvent
+} from "vscode";
 
 import TestRunnerDebugCodeLens from "../codelens/TestDebugRunnerCodeLens";
 import TestRunnerCodeLens from "../codelens/TestRunnerCodeLens";
 import { codeParser } from "../parser/codeParser";
+import { parseTestFrameworks } from "../parser/testFrameworkParser";
 
 function getRootPath({ uri }) {
   const activeWorkspace = workspace.getWorkspaceFolder(uri);
@@ -33,6 +41,37 @@ function getCodeLens(rootPath, fileName, testName, startPosition) {
 }
 
 export default class TestRunnerCodeLensProvider implements CodeLensProvider {
+  constructor() {
+    // Register workspace folder change listener to detect added/removed workspaces during runtime
+    workspace.onDidChangeWorkspaceFolders(this.onWorkspaceChanged);
+    // Check existing workspace for mocha/jest dependency
+    workspace.workspaceFolders.forEach(ws => {
+      parseTestFrameworks(ws)
+        .then(r => {
+          window.showInformationMessage(
+            "Found " +
+              r.enabledFrameworks.length +
+              " enabled test frameworks in workspace folder " +
+              ws.uri.path
+          );
+        })
+        .catch(e =>
+          window.showErrorMessage(
+            "Error while trying to retrieve test frameworks for workspace folder " +
+              ws.uri.path +
+              ": " +
+              e
+          )
+        );
+    });
+    const packageJsonWatcher = workspace.createFileSystemWatcher(
+      "**/package.json"
+    );
+    packageJsonWatcher.onDidChange(this.onPackageJsonChanged);
+    packageJsonWatcher.onDidCreate(this.onPackageJsonCreated);
+    packageJsonWatcher.onDidDelete(this.onPackageJsonDeleted);
+  }
+
   public provideCodeLenses(
     document: TextDocument
   ): CodeLens[] | Thenable<CodeLens[]> {
@@ -55,5 +94,21 @@ export default class TestRunnerCodeLensProvider implements CodeLensProvider {
 
   public resolveCodeLens?(): CodeLens | Thenable<CodeLens> {
     return;
+  }
+
+  private onWorkspaceChanged(event: WorkspaceFoldersChangeEvent) {
+    window.showInformationMessage("Worspace was changed");
+  }
+
+  private onPackageJsonChanged(uri) {
+    window.showInformationMessage(uri + " was changed");
+  }
+
+  private onPackageJsonCreated(uri) {
+    window.showInformationMessage(uri + " was created");
+  }
+
+  private onPackageJsonDeleted(uri) {
+    window.showInformationMessage(uri + " was deleted");
   }
 }
