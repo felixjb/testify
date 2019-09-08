2432d022b7b130aa467d8eef79c97a8a7e4f7ecc
felixjb <felixjbcomp@gmail.com>
2019-09-08 15:31:47 -0300

    Implement custom test runner path
    Use configuration for non default test runner path to instantiate test
    runner

--- src/interfaces/ITestRunnerOptions.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigurationProvider } from "../providers/ConfigurationProvider";
-import { TerminalProvider } from "../providers/TerminalProvider";
-
-export interface ITestRunnerOptions {
-  terminalProvider: TerminalProvider;
-  configurationProvider: ConfigurationProvider;
-}
