d7a7c6876f52657dc8ebfa4e422302fc9b00c632
LoveSponge <guy.kellermann@gmail.com>
2020-05-15 23:56:59 +0100

    Added configuration option to pakcage.json. Added configuration detection. Amended codeLensSubscriptions to only show lenses on accepted files, based on above configuration.

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,53 @@
-import { commands, ExtensionContext, languages } from "vscode";
+import { commands, ExtensionContext, languages, workspace } from "vscode";
+import { ConfigurationProvider } from "./providers/ConfigurationProvider";
 import debugTestCommand from "./commands/debugTestCommand";
 import runTestCommand from "./commands/runTestCommand";
-import FILE_SELECTOR from "./constants/fileSelector";
 import TestRunnerCodeLensProvider from "./providers/TestRunnerCodeLensProvider";
 
 export function activate(context: ExtensionContext) {
-  context.subscriptions.push(
-    languages.registerCodeLensProvider(
-      FILE_SELECTOR,
+  function createCodeLensSubscription(fileSelector) {
+    if (!fileSelector) {
+      return;
+    }
+
+    return languages.registerCodeLensProvider(
+      fileSelector,
       new TestRunnerCodeLensProvider()
-    )
-  );
+    );
+  }
+
+  let codeLensSubscription = createCodeLensSubscription(getExecutionContext());
+
+  if (!codeLensSubscription) {
+    return;
+  } // If the current vscode context has no open folder
+  context.subscriptions.push(codeLensSubscription);
 
   commands.registerCommand("testify.run.test", runTestCommand);
   commands.registerCommand("testify.debug.test", debugTestCommand);
+
+  workspace.onDidChangeConfiguration(() => {
+    codeLensSubscription.dispose();
+    codeLensSubscription = createCodeLensSubscription(getExecutionContext());
+    context.subscriptions.push();
+  });
+}
+
+function getExecutionContext() {
+  const x = workspace.workspaceFolders;
+  if (!x) {
+    return;
+  }
+
+  const { configuration } = new ConfigurationProvider(
+    workspace.workspaceFolders[0]
+  );
+  const pattern = configuration.testFilePattern;
+
+  return [
+    {
+      pattern,
+      scheme: "file"
+    }
+  ];
 }
