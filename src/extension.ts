f16d51112b0a5fad3614ed5fb1c910ccee8532a7
LoveSponge <guy.kellermann@gmail.com>
2020-05-20 00:38:07 +0100

    Updated code

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,50 @@
 import { commands, ExtensionContext, languages, workspace } from "vscode";
 import debugTestCommand from "./commands/debugTestCommand";
 import runTestCommand from "./commands/runTestCommand";
-import {
-  createCodeLensSubscription,
-  getExecutionContext
-} from "./util/getExecutionContext";
+import TestRunnerCodeLensProvider from "./providers/TestRunnerCodeLensProvider";
 
 export function activate(context: ExtensionContext) {
-  let codeLensSubscription = createCodeLensSubscription(
-    languages,
-    getExecutionContext(workspace)
-  );
-
-  if (!codeLensSubscription) {
-    return;
-  } // If the current vscode context has no open folder
-  context.subscriptions.push(codeLensSubscription);
-
-  workspace.onDidChangeConfiguration(() => {
-    codeLensSubscription.dispose();
-    codeLensSubscription = createCodeLensSubscription(
-      languages,
-      getExecutionContext(workspace)
-    );
-    context.subscriptions.push(codeLensSubscription);
-  });
+  const reregisterTestifyLens = registerTestingLens(context);
+  // whenever the user configuration is changed, reregister the extension
+  workspace.onDidChangeConfiguration(reregisterTestifyLens);
 
   commands.registerCommand("testify.run.test", runTestCommand);
   commands.registerCommand("testify.debug.test", debugTestCommand);
+}
+
+function registerTestingLens(context: ExtensionContext) {
+  // automatically register testify codelens
+  // return a function that when run will dispose of and re-register the codelens.
+  const lensProvider = new TestRunnerCodeLensProvider();
+  const subscription = register(getFileSelector());
+
+  function register(fileSelector) {
+    // create a new subscription to our beautiful lenses
+    // push them on to the current context.subscriptions array
+    const newSubscription = languages.registerCodeLensProvider(
+      fileSelector,
+      lensProvider
+    );
+
+    context.subscriptions.push(newSubscription);
+    return newSubscription;
+  }
+
+  function getFileSelector() {
+    // this allows a fast and easy way to get the extension's configurations.
+    // doesn't care about open workspace/folder
+    const { testFilePattern } = workspace.getConfiguration("testify");
+
+    return {
+      pattern: testFilePattern,
+      scheme: "file"
+    };
+  }
+
+  return function reregister() {
+    // out with the old, in with the new
+    const updatedFileSelector = getFileSelector();
+    subscription.dispose();
+    register(updatedFileSelector);
+  };
 }
