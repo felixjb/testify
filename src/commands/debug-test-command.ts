4d5eb031cb9c98e115229d65c3362b438c7a318b
felixjb <felixjbcomp@gmail.com>
2024-04-19 22:30:39 +0200

    refactor: make inline export default

--- src/commands/debug-test-command.ts
+++ src/commands/debug-test-command.ts
@@ -2,11 +2,13 @@ import {relative} from 'path'
 import {WorkspaceFolder} from 'vscode'
 import {getTestRunner} from '../runners/test-runner-factory'
 
-async function debugTest(rootPath: WorkspaceFolder, fileName: string, testName: string) {
+export default async function debugTest(
+  rootPath: WorkspaceFolder,
+  fileName: string,
+  testName: string
+): Promise<void> {
   const relativeFilename = relative(rootPath.uri.fsPath, fileName)
   const testRunner = await getTestRunner(rootPath)
 
   testRunner.debugTest(rootPath, relativeFilename, testName)
 }
-
-export default debugTest
--- src/commands/run-test-command.ts
+++ src/commands/run-test-command.ts
@@ -2,11 +2,13 @@ import {relative} from 'path'
 import {WorkspaceFolder} from 'vscode'
 import {getTestRunner} from '../runners/test-runner-factory'
 
-async function runTest(rootPath: WorkspaceFolder, fileName: string, testName: string) {
+export default async function runTest(
+  rootPath: WorkspaceFolder,
+  fileName: string,
+  testName: string
+): Promise<void> {
   const relativeFilename = relative(rootPath.uri.fsPath, fileName)
   const testRunner = await getTestRunner(rootPath)
 
   testRunner.runTest(rootPath, relativeFilename, testName)
 }
-
-export default runTest
